fcdb10dbeedb15af8dee965006eccc6c3ef2b43c
xLelouch03 <hans_rejano@dlsu.edu.ph>
2022-10-23 12:55:31 +0800

    modified

--- src/Seed.java
+++ src/Seed.java
@@ -1,9 +1,44 @@
 public class Seed {
     private String name;
+    private String type;
+    private int harvestTime;
+    private int waterNeed;
+    private int fertilizerNeed;
+    private int productProduced;
     private int cost;
+    private double sellingPrice;
+    private double experience;
 
-    public Seed(String name, int cost) {
+    public Seed(String name, String type, int harvestTime, int waterNeed, int fertilizerNeed, int cost) {
         this.name = name;
+        this.type = type;
+        this.harvestTime = harvestTime;
+        this.waterNeed = waterNeed;
+        this.fertilizerNeed = fertilizerNeed;
         this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getHarvestTime() {
+        return harvestTime;
+    }
+
+    public int getWaterNeed() {
+        return waterNeed;
+    }
+
+    public int getFertilizerNeed() {
+        return fertilizerNeed;
+    }
+
+    public int getCost() {
+        return cost;
     }
 }
