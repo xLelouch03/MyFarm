f5093abfe4fbaf3f64b77d2390390720fcfbf1be
xLelouch03 <hans_rejano@dlsu.edu.ph>
2022-10-23 12:50:08 +0800

    modified

--- src/Tool.java
+++ src/Tool.java
@@ -1,3 +1,22 @@
 public class Tool {
-    
+    private String name;
+    private int toolCost;
+    private double experience;
+
+    public Tool(String name, int toolCost, double experience) {
+        this.name = name;
+        this.toolCost = toolCost;
+        this.experience = experience;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getCost() {
+        return toolCost;
+    }
+
+    public double getXP() {
+        return experience;
+    }
 }
