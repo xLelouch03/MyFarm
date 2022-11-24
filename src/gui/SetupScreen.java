7eb4f92656378ae5bef826abf635eff289eff702
xLelouch03 <hans_rejano@dlsu.edu.ph>
2022-11-24 22:22:49 +0800

    added image background

--- src/gui/SetupScreen.java
+++ src/gui/SetupScreen.java
@@ -1,5 +1,7 @@
 package gui;
+import main.Main;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,6 +13,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SetupScreen {
+    private Main player;
     private JFrame setupFrame;
 
     public SetupScreen() {
@@ -19,26 +22,26 @@ public class SetupScreen {
 
     public void initialize() {
         setupFrame = new JFrame("My Farm - Farming Simulation Game");
-        setupFrame.setBounds(100, 100, 600, 325);
+        setupFrame.setBounds(100, 100, 600, 280);
 		setupFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setupFrame.setContentPane(new JLabel(new ImageIcon("D:\\User\\Documents\\GitHub\\MyFarm\\src\\assets\\farm.jpg")));
 		setupFrame.getContentPane().setLayout(null);
 
         JLabel welcomeLabel = new JLabel("Welcome to your farm!");
         welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		welcomeLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-		welcomeLabel.setBounds(10, 0, 575, 44);
+		welcomeLabel.setBounds(10, 30, 575, 44);
 		setupFrame.getContentPane().add(welcomeLabel);
 
         JLabel farmerNameLabel = new JLabel("What is your name?");
 		farmerNameLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		farmerNameLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
-		farmerNameLabel.setBounds(20, 45, 575, 44);
+		farmerNameLabel.setBounds(35, 90, 575, 44);
 		setupFrame.getContentPane().add(farmerNameLabel);
 
         JTextField farmerNameTextField = new JTextField();
 		farmerNameTextField.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		farmerNameTextField.setBounds(225, 50, 300, 39);
-        farmerNameTextField.setColumns(5);
+		farmerNameTextField.setBounds(235, 95, 300, 39);
 		setupFrame.getContentPane().add(farmerNameTextField);
 
         JButton startButton = new JButton("Start");
@@ -46,16 +49,19 @@ public class SetupScreen {
 		{
 			public void actionPerformed(ActionEvent e) 
 			{
-	
+                //player.setupGame(farmerNameTextField.getText());
 			}
 		});
 
         startButton.setFont(new Font("Tahoma", Font.BOLD, 16));
-		startButton.setBounds(215, 125, 160, 45);
+		startButton.setBounds(215, 170, 160, 45);
 		setupFrame.getContentPane().add(startButton);
 
-        JLabel authorLabel = new JLabel();
         setupFrame.setVisible(true);
+    }
+
+    public void close() {
+        setupFrame.dispose();
     }
 
     public static void main(String[] args) {
